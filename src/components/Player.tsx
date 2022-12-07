888b0f9b356190ba4b673c6266d66a96f67e556f
DimaMakarenko <makarenko.d.viri@gmail.com>
2022-12-07 12:21:59 +0200

    feat: player moves

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -2,15 +2,21 @@ import { useThree, useFrame } from "@react-three/fiber";
 import { useSphere } from "@react-three/cannon";
 import { FC, useEffect, useRef } from "react";
 import { Vector3 } from "three";
+import { useKeyboard } from "../hooks/useKeyboard";
 
 interface IPlayer {}
 
+const JUMP_FORCE = 4;
+const SPEED = 4;
+
 export const Player: FC<IPlayer> = () => {
+  const actions = useKeyboard();
+
   const { camera } = useThree();
   const [ref, api] = useSphere(() => ({
-    mess: 1,
+    mass: 1,
     type: "Dynamic",
-    position: [0, 4, 0],
+    position: [0, 1, 0],
   }));
 
   const velocity = useRef([0, 0, 0]);
@@ -30,7 +36,30 @@ export const Player: FC<IPlayer> = () => {
       new Vector3(position.current[0], position.current[1], position.current[2])
     );
 
-    api.velocity.set(0, 0, 0);
+    const direction = new Vector3();
+    const frontVector = new Vector3(
+      0,
+      0,
+      (actions.moveBackward ? 1 : 0) - (actions.moveForward ? 1 : 0)
+    );
+    const sideVector = new Vector3(
+      (actions.moveLeft ? 1 : 0) - (actions.moveRight ? 1 : 0),
+      0,
+      0
+    );
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(SPEED)
+      .applyEuler(camera.rotation);
+
+      api.velocity.set(direction.x, velocity.current[1], direction.z);
+
+    if (actions.jump && Math.abs(velocity.current[1]) < 0.05) {
+      api.velocity.set(velocity.current[0], JUMP_FORCE, velocity.current[2]);
+    }
   });
+  
   return <mesh ref={ref}></mesh>;
 };
---
+++ src/hooks/useKeyboard.ts
@@ -0,0 +1,74 @@
+import { useCallback, useEffect, useState } from "react"
+
+
+enum Keys {
+  KeyW = 'KeyW',
+  KeyS = 'KeyS',
+  KeyA = 'KeyA',
+  KeyD = 'KeyD',
+  Space = 'Space',
+  Dirt = 'Dirt',
+  Grass = 'Grass',
+  Glass = 'Glass',
+  Wood = 'Wood',
+  Log = 'Log',
+}
+type IKeyActionMap = Record<string, string>
+
+const actionByKey = (key: string) => {
+  const keyActionMap: IKeyActionMap = {
+    KeyW: 'moveForward',
+    KeyS: 'moveBackward',
+    KeyA: 'moveLeft',
+    KeyD: 'moveRight',
+    Space: 'jump',
+    Dirt: 'dirt',
+    Grass: 'grass',
+    Glass: 'glass',
+    Wood: 'wood',
+    Log: 'log',
+  }
+
+  return keyActionMap[key];
+}
+
+export const useKeyboard = () => {
+  const [actions, setActions] = useState({
+    moveForward: false,
+    moveBackward: false,
+    moveLeft: false,
+    moveRight: false,
+    jump: false,
+    dirt: false,
+    grass: false,
+    glass: false,
+    wood: false,
+    log: false,
+  });
+
+  const handleKeyDown = useCallback((e: KeyboardEvent) => {
+    const action = actionByKey(e.code)
+    if (action) {
+      setActions(prev => ({ ...prev, [action]: true }))
+    }
+  }, []);
+
+  const handleKeyUp = useCallback((e: KeyboardEvent) => {
+    const action = actionByKey(e.code)
+    if (action) {
+      setActions(prev => ({ ...prev, [action]: false }))
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
+    }
+  }, [handleKeyDown, handleKeyUp]);
+
+  return actions
+}
