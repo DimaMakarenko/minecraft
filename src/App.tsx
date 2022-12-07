e16c91d4fd88ae1fa1ffe0c499e80d4162654ce8
DimaMakarenko <makarenko.d.viri@gmail.com>
2022-12-07 10:48:03 +0200

    feat: ground component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas } from "@react-three/fiber";
+import { Sky } from "@react-three/drei";
+import { Physics } from "@react-three/cannon";
+import { Ground } from "./components/Ground";
 
 function App() {
   return (
     <>
-      <div>Outside Canvas</div>
       <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Ground />
+        </Physics>
       </Canvas>
     </>
   );
---
+++ src/components/Ground.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import { usePlane } from "@react-three/cannon";
+import { groundTexture } from "../assets/textures";
+import { RepeatWrapping, NearestFilter } from "three";
+
+interface IGround {}
+
+export const Ground: FC<IGround> = () => {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 3, 0, 0],
+    position: [0, 0, 0],
+  }));
+
+  groundTexture.magFilter = NearestFilter;
+  groundTexture.wrapS = RepeatWrapping;
+  groundTexture.wrapT = RepeatWrapping;
+  groundTexture.repeat.set(100, 100);
+
+  return (
+    <mesh ref={ref}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshStandardMaterial attach="material" map={groundTexture} />
+    </mesh>
+  );
+};
